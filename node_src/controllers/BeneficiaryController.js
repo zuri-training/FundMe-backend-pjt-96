9f756ef38f03b61380fbfe531df10dbfd003c323
Sobowale-Olayiwola <layitheinfotechguru@gmail.com>
2021-06-21 20:39:12 +0100

    Added Beneficiary controller file

---
+++ node_src/controllers/BeneficiaryController.js
@@ -0,0 +1,19 @@
+const BeneficiaryService = require("../services/BeneficiaryService");
+module.exports = class BeneficiaryController {
+  static async getAllBeneficiaries(request, response) {
+    try {
+      let beneficiaries = await BeneficiaryService.getAllBeneficiaries();
+      response
+        .status(200)
+        .json({ code: "SUCCESS", success: beneficiaries, error: null });
+    } catch (error) {
+      response.status(500).json({
+        code: "FAILED",
+        success: null,
+        error:
+          error.message ||
+          " You cannot get all the list of Students at the moment",
+      });
+    }
+  }
+};
