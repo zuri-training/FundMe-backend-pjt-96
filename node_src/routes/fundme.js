cdabc7a188d732e2dc4cae232d8f48159ee72eb8
William Imoukhuede <wimoukhuede@gmail.com>
2021-06-23 23:14:47 +0100

    Revert "Testing hosting" pls This reverts commit 67d046892afeb809741f45e5b03f620b03e6bb8e.

--- node_src/routes/fundme.js
+++ node_src/routes/fundme.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const undergraduate = require("../routes/undergraduates");
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'It works'})
-})
 router.use("/", undergraduate);
 
 module.exports = router;
