67d046892afeb809741f45e5b03f620b03e6bb8e
William Imoukhuede <wimoukhuede@gmail.com>
2021-06-23 23:11:59 +0100

    Testing hosting

--- node_src/routes/fundme.js
+++ node_src/routes/fundme.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const undergraduate = require("../routes/undergraduates");
 
+router.get('/', (req, res) => {
+    res.status(200).json({message: 'It works'})
+})
 router.use("/", undergraduate);
 
 module.exports = router;
